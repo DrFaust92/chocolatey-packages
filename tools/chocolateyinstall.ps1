8947c36a497212c0c2069dd6fe1f68a72912498d
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2019-03-26 16:53:41 +0200

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,9 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
  
 $packageName = 'terragrunt'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$url      = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.18.0/terragrunt_windows_amd64.exe'
+$checksum = '6eaadb67269a598893e0a8dadad9054f4ca3186b8c0f94aecdb38362a2042cb0'
+$url      = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.18.2/terragrunt_windows_amd64.exe'
 $destination = "$toolsPath\terragrunt_windows_amd64.exe"
 
 if (Test-Path "$toolsPath\terragrunt.exe") {
@@ -10,5 +11,5 @@ if (Test-Path "$toolsPath\terragrunt.exe") {
   Uninstall-BinFile -Name "terragrunt" -Path "$toolsPath"
 }
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
 Rename-Item -Path "$toolsPath\terragrunt_windows_amd64.exe" -NewName "terragrunt.exe"
