4e023ee01ff60ec3f9cbf8050086dcc7be4c1589
DrFaust92 <ilia.lazebnik@gmail.com>
2020-03-04 13:36:09 +0200

    add terraformer

---
+++ terraformer/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = "Stop"
+write-host "Attempting to stop any running terraformer processes, to allow upgrade"
+try
+{
+    $running = get-process terraformer
+}
+catch
+{
+    write-host "No running terraformer processes to stop"
+    $running = @()
+}
+
+if ($running)
+{
+    write-host "Stopping terraformer processes"
+    foreach ($p in $running)
+    {
+        stop-process $p
+        write-verbose "Stopped $( $p.ProcessName ) $( $p.Id )"
+    }
+}
