802e5dde6f145c91f1d0a7e2e23017ad41c86795
DrFaust92 <ilia.lazebnik@gmail.com>
2020-03-08 11:19:16 +0200

    tfsec simplify scripts + add icon and sha256

--- tfsec/tools/chocolateyinstall.ps1
+++ tfsec/tools/chocolateyinstall.ps1
@@ -1,13 +1,9 @@
 $ErrorActionPreference = 'Stop'
  
 $packageName = 'tfsec'
+$checksum = 'a264df87033c5046e42ef2189fcdb45fa362b434b76e686eaca77fb5c850f345'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $url      = 'https://github.com/liamg/tfsec/releases/download/v0.19.0/tfsec-windows-amd64.exe'
 $destination = "$toolsPath\tfsec.exe"
 
-if (Test-Path "$toolsPath\tfsec.exe") {
-  Remove-Item "$toolsPath\tfsec.exe"
-  Uninstall-BinFile -Name "tfsec" -Path "$toolsPath"
-}
-
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
