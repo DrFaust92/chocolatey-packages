8139865a43d35a30383e83c9566a21e90912e8af
DrFaust92 <ilia.lazebnik@gmail.com>
2020-03-12 21:15:24 +0200

    update kubergrunt to 0.5.11

--- kubergrunt/tools/chocolateyinstall.ps1
+++ kubergrunt/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
- 
+
 $packageName = 'kubergrunt'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = 'b973add2f07461b43a271b1378dbba1ea6f0a42adc9f29fad87fc0e77098aa46'
-$url      = 'https://github.com/gruntwork-io/kubergrunt/releases/download/v0.5.10/kubergrunt_windows_amd64.exe'
+$checksum = 'aa23b93772ea1ffa86b01e498e402ec061abe8baf050f2863e94ff549b4bf044'
+$url = 'https://github.com/gruntwork-io/kubergrunt/releases/download/v0.5.11/kubergrunt_windows_amd64.exe'
 $destination = "$toolsPath\kubergrunt.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
