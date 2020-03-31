e14e49f7af50cf427f70994b2858868b034f6367
DrFaust92 <ilia.lazebnik@gmail.com>
2020-04-01 00:30:37 +0300

    update terragrunt to 0.23.4

--- terragrunt/tools/chocolateyinstall.ps1
+++ terragrunt/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'terragrunt'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = 'e84810cfcc1c77c03dfbaeb71114c54136080026c64efe621ac5ccdec9b8a219'
-$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.23.2/terragrunt_windows_amd64.exe'
+$checksum = 'fc3949f230abcdd76946a1f8f16333ada6e131a4e01c0cf7e6acf8a7da5ab88c'
+$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.23.4/terragrunt_windows_amd64.exe'
 $destination = "$toolsPath\terragrunt.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
