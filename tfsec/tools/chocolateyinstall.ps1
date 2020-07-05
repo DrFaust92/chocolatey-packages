fad226e715b73dc5d13bab0e06ab7c9e7ab52d26
DrFaust92 <ilia.lazebnik@gmail.com>
2020-07-05 10:33:14 +0300

    updates: * tfsec to 0.23.0

--- tfsec/tools/chocolateyinstall.ps1
+++ tfsec/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
- 
+
 $packageName = 'tfsec'
-$checksum = 'e264cb3020e9857ca64a79448c31dfa7811fbdb2d4fc94e74e25257012c53241'
+$checksum = '50f13229b58d339e2fcc49d7ff65dd2c460725c9dd3377866d98dab5b7540bb6'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$url = 'https://github.com/liamg/tfsec/releases/download/v0.21.0/tfsec-windows-amd64.exe'
+$url = 'https://github.com/liamg/tfsec/releases/download/v0.23.0/tfsec-windows-amd64.exe'
 $destination = "$toolsPath\tfsec.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
