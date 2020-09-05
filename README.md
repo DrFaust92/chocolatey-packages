a74fb5e09b8a9db274bd64771c85ac7614afb4ee
DrFaust92 <ilia.lazebnik@gmail.com>
2020-09-05 17:12:08 +0300

    update terragrunt to v0.23.40

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 
 ## Terragrunt
-The current version is 0.23.38, install it using choco:
+The current version is 0.23.40, install it using choco:
 ```
 choco install terragrunt
 ```
--- terragrunt/tools/chocolateyinstall.ps1
+++ terragrunt/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'terragrunt'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = '12451509decb121eeecc481922ee164bea7684306b3884116e0f7f79bbe6291a'
-$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.23.38/terragrunt_windows_amd64.exe'
+$checksum = '038a46a195da587affe400f77a020d0946ed559c0923b6890788acd538155b2e'
+$url = 'https://github.com/gruntwork-io/terragrunt/releases/download/v0.23.40/terragrunt_windows_amd64.exe'
 $destination = "$toolsPath\terragrunt.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
